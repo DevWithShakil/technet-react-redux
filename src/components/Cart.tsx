9358c3deda1fe01bfe253fd294cf6e26bd7596b2
Shakil Khan <m.shakilkhan720@gmail.com>
2023-07-07 11:41:51 +0600

    Cart add and reduce product complete

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -13,11 +13,18 @@ import {
 } from 'react-icons/hi';
 import { Button } from './ui/button';
 import { IProduct } from '@/types/globalTypes';
+import { useAppDispatch, useAppSelector } from '@/redux/hook';
+import {
+  addToCart,
+  removeFromCart,
+  removeOne,
+} from '@/redux/features/cart/cartSlice';
 
 export default function Cart() {
-  //! Dummy data
+  const { products } = useAppSelector((state) => state.cart);
+  const dispatch = useAppDispatch();
 
-  const products: IProduct[] = [];
+  //! Dummy data
   const total = 0;
 
   //! **
@@ -52,13 +59,14 @@ export default function Cart() {
                 </p>
               </div>
               <div className="border-l pl-5 flex flex-col justify-between">
-                <Button>
+                <Button onClick={() => dispatch(addToCart(product))}>
                   <HiOutlinePlus size="20" />
                 </Button>
-                <Button>
+                <Button onClick={() => dispatch(removeOne(product))}>
                   <HiMinus size="20" />
                 </Button>
                 <Button
+                  onClick={() => dispatch(removeFromCart(product))}
                   variant="destructive"
                   className="bg-red-500 hover:bg-red-400"
                 >
--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { RouterProvider } from 'react-router-dom';
 import routes from './routes/routes.tsx';
+import { Provider } from 'react-redux';
+import store from './redux/store.ts';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <RouterProvider router={routes} />
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <RouterProvider router={routes} />
+    </React.StrictMode>
+  </Provider>
 );
